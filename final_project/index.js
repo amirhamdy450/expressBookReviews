ccd2e88f90afa0be5858f915af416eefa87708e8
amir amgad <155030172+amirhamdy450@users.noreply.github.com>
2024-04-29 17:12:05 +0300

    Add files via upload

--- final_project/index.js
+++ final_project/index.js
@@ -10,10 +10,24 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+  if (req.session.authorization) {
+    const token = req.session.authorization.accessToken; // Retrieve the token from the session
+    if (!token) {
+      return res.status(401).send("Unauthorized: No token provided");
+    }
+
+    jwt.verify(token, 'access', (err, decoded) => {
+      if (err) {
+        return res.status(403).send("Unauthorized: Invalid token");
+      }
+      req.username = decoded.data;
+      next();
+    });
+  } else {
+    return res.status(401).send("Unauthorized: Authorization Data Not Found");
+  }
 });
- 
 const PORT =5000;
 
 app.use("/customer", customer_routes);
